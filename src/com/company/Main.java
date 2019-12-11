18cf91d2a69de29f81992dca680e2fc51a15886f
Lusin-mus <lusnarmus@gmail.com>
2019-12-11 15:06:33 +0400

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,24 @@ public class Main {
     public static void main(String[] args) {
         String greeting = "hello world";
         int length = greeting.length();
-
-        for (int i = 0; i < length; i++) {
-            int ch = greeting.charAt(i);
-            if(ch != ' ') {
-                ch = ch - 32;
-                String firstPart = greeting.substring(0, i);
-                String secondPart = greeting.substring(i + 1);
-                System.out.println(firstPart + (char)ch + secondPart);
+        while (true) {
+            for (int i = 0; i < length; i++) {
+                int ch = greeting.charAt(i);
+                if (ch != ' ') {
+                    ch = ch - 32;
+                    String firstPart = greeting.substring(0, i);
+                    String secondPart = greeting.substring(i + 1);
+                    System.out.println(firstPart + (char) ch + secondPart);
+                }
+            }
+            for (int j = greeting.length() - 2; j > 0; j--) {
+                int ch = greeting.charAt(j);
+                if (ch != ' ') {
+                    ch = ch - 32;
+                    String firstPart = greeting.substring(0, j);
+                    String secondPart = greeting.substring(j + 1);
+                    System.out.println(firstPart + (char) ch + secondPart);
+                }
             }
         }
     }
